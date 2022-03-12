4e7a012e0b0ab1a1e3380ee98b4f3eaec2e171d2
Sedyankoff <stoil0878@gmail.com>
2022-03-12 21:10:56 +0200

    Added a missing button in Main Courses Design.

--- RecipesCatalog/Forms/MainCoursesForm.Designer.cs
+++ RecipesCatalog/Forms/MainCoursesForm.Designer.cs
@@ -37,6 +37,7 @@ namespace RecipesCatalog.Forms
             this.btnMainCoursesUnfavourite = new System.Windows.Forms.Button();
             this.picBoxMainCourses = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnMainCoursesFavourite = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataMainCourses)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMainCourses)).BeginInit();
             this.SuspendLayout();
@@ -110,11 +111,23 @@ namespace RecipesCatalog.Forms
             this.label1.TabIndex = 15;
             this.label1.Text = "Main Courses Recipes:";
             // 
+            // btnMainCoursesFavourite
+            // 
+            this.btnMainCoursesFavourite.FlatAppearance.BorderSize = 0;
+            this.btnMainCoursesFavourite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMainCoursesFavourite.Image = ((System.Drawing.Image)(resources.GetObject("btnMainCoursesFavourite.Image")));
+            this.btnMainCoursesFavourite.Location = new System.Drawing.Point(370, 403);
+            this.btnMainCoursesFavourite.Name = "btnMainCoursesFavourite";
+            this.btnMainCoursesFavourite.Size = new System.Drawing.Size(50, 40);
+            this.btnMainCoursesFavourite.TabIndex = 16;
+            this.btnMainCoursesFavourite.UseVisualStyleBackColor = true;
+            // 
             // MainCoursesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(820, 471);
+            this.Controls.Add(this.btnMainCoursesFavourite);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnMainCoursesUnfavourite);
             this.Controls.Add(this.picBoxMainCourses);
@@ -140,5 +153,6 @@ namespace RecipesCatalog.Forms
         private System.Windows.Forms.Button btnOpenMainCourses;
         private System.Windows.Forms.DataGridView dataMainCourses;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMainCoursesFavourite;
     }
 }
