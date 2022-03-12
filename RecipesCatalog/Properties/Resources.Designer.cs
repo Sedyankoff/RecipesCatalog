b9ac9a72273fa85bb671fc10453d46c5bf792f7a
Мартин Кондакчиев <mk88923724@edu.mon.bg>
2022-03-12 17:35:11 +0200

    Added Favourites design.

--- RecipesCatalog/Properties/Resources.Designer.cs
+++ RecipesCatalog/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace RecipesCatalog.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap logoFinished {
+            get {
+                object obj = ResourceManager.GetObject("logoFinished", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap meals {
             get {
                 object obj = ResourceManager.GetObject("meals", resourceCulture);
