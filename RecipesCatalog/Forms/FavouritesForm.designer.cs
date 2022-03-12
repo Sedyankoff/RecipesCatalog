8589cf26781e0d2c05e18128deb69b7da53f67a1
Stoyanowww <pstoyanov04@gmail.com>
2022-03-12 17:41:50 +0200

    Change Favourites backround.

--- RecipesCatalog/Forms/FavouritesForm.designer.cs
+++ RecipesCatalog/Forms/FavouritesForm.designer.cs
@@ -90,6 +90,7 @@ namespace RecipesCatalog.Forms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
