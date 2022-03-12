7fd2be19662c3d1bf3aa7963be7794968e35b8b5
Stoqnowww <pstoyanov04@gmail.com>
2022-03-12 09:27:40 +0200

    Image change.

--- RecipesCatalog/Form1.Designer.cs
+++ RecipesCatalog/Form1.Designer.cs
@@ -31,18 +31,18 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.panelLogo = new System.Windows.Forms.Panel();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.panelButtons = new System.Windows.Forms.Panel();
-            this.panelTitle = new System.Windows.Forms.Panel();
-            this.btnHome = new System.Windows.Forms.Button();
             this.btnFavourites = new System.Windows.Forms.Button();
             this.btnAddRecipe = new System.Windows.Forms.Button();
             this.btnCourses = new System.Windows.Forms.Button();
             this.btnMain = new System.Windows.Forms.Button();
             this.btnAppetizers = new System.Windows.Forms.Button();
             this.btnSalads = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.Button();
             this.panelMenu.SuspendLayout();
             this.panelLogo.SuspendLayout();
             this.panelTitle.SuspendLayout();
@@ -64,6 +64,112 @@
             this.panelMenu.Size = new System.Drawing.Size(200, 590);
             this.panelMenu.TabIndex = 0;
             // 
+            // btnFavourites
+            // 
+            this.btnFavourites.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFavourites.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFavourites.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
+            this.btnFavourites.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFavourites.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFavourites.ForeColor = System.Drawing.Color.White;
+            this.btnFavourites.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFavourites.Location = new System.Drawing.Point(0, 380);
+            this.btnFavourites.Name = "btnFavourites";
+            this.btnFavourites.Size = new System.Drawing.Size(200, 60);
+            this.btnFavourites.TabIndex = 6;
+            this.btnFavourites.Text = " Favourites";
+            this.btnFavourites.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnFavourites.UseVisualStyleBackColor = true;
+            // 
+            // btnAddRecipe
+            // 
+            this.btnAddRecipe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddRecipe.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAddRecipe.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
+            this.btnAddRecipe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddRecipe.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddRecipe.ForeColor = System.Drawing.Color.White;
+            this.btnAddRecipe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAddRecipe.Location = new System.Drawing.Point(0, 320);
+            this.btnAddRecipe.Name = "btnAddRecipe";
+            this.btnAddRecipe.Size = new System.Drawing.Size(200, 60);
+            this.btnAddRecipe.TabIndex = 5;
+            this.btnAddRecipe.Text = " Add Recipe";
+            this.btnAddRecipe.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAddRecipe.UseVisualStyleBackColor = true;
+            // 
+            // btnCourses
+            // 
+            this.btnCourses.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCourses.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCourses.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
+            this.btnCourses.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCourses.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCourses.ForeColor = System.Drawing.Color.White;
+            this.btnCourses.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCourses.Location = new System.Drawing.Point(0, 260);
+            this.btnCourses.Name = "btnCourses";
+            this.btnCourses.Size = new System.Drawing.Size(200, 60);
+            this.btnCourses.TabIndex = 4;
+            this.btnCourses.Text = " Deserts";
+            this.btnCourses.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCourses.UseVisualStyleBackColor = true;
+            // 
+            // btnMain
+            // 
+            this.btnMain.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMain.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
+            this.btnMain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMain.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMain.ForeColor = System.Drawing.Color.White;
+            this.btnMain.Image = ((System.Drawing.Image)(resources.GetObject("btnMain.Image")));
+            this.btnMain.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMain.Location = new System.Drawing.Point(0, 200);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(200, 60);
+            this.btnMain.TabIndex = 3;
+            this.btnMain.Text = " Main Courses";
+            this.btnMain.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMain.UseVisualStyleBackColor = true;
+            // 
+            // btnAppetizers
+            // 
+            this.btnAppetizers.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAppetizers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAppetizers.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
+            this.btnAppetizers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAppetizers.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppetizers.ForeColor = System.Drawing.Color.White;
+            this.btnAppetizers.Image = ((System.Drawing.Image)(resources.GetObject("btnAppetizers.Image")));
+            this.btnAppetizers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAppetizers.Location = new System.Drawing.Point(0, 140);
+            this.btnAppetizers.Name = "btnAppetizers";
+            this.btnAppetizers.Size = new System.Drawing.Size(200, 60);
+            this.btnAppetizers.TabIndex = 2;
+            this.btnAppetizers.Text = " Appetizers";
+            this.btnAppetizers.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAppetizers.UseVisualStyleBackColor = true;
+            // 
+            // btnSalads
+            // 
+            this.btnSalads.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalads.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSalads.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
+            this.btnSalads.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalads.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalads.ForeColor = System.Drawing.Color.White;
+            this.btnSalads.Image = ((System.Drawing.Image)(resources.GetObject("btnSalads.Image")));
+            this.btnSalads.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalads.Location = new System.Drawing.Point(0, 80);
+            this.btnSalads.Name = "btnSalads";
+            this.btnSalads.Size = new System.Drawing.Size(200, 60);
+            this.btnSalads.TabIndex = 1;
+            this.btnSalads.Text = " Salads";
+            this.btnSalads.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSalads.UseVisualStyleBackColor = true;
+            this.btnSalads.Click += new System.EventHandler(this.btnSalads_Click);
+            // 
             // panelLogo
             // 
             this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
@@ -73,6 +179,19 @@
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(200, 80);
             this.panelLogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "    Catalog";
             // 
             // imageList1
             // 
@@ -116,129 +235,6 @@
             this.btnHome.Text = "HOME";
             this.btnHome.UseVisualStyleBackColor = true;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
-            // 
-            // btnFavourites
-            // 
-            this.btnFavourites.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnFavourites.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnFavourites.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
-            this.btnFavourites.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFavourites.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFavourites.ForeColor = System.Drawing.Color.White;
-            this.btnFavourites.Image = global::RecipesCatalog.Properties.Resources.rating;
-            this.btnFavourites.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnFavourites.Location = new System.Drawing.Point(0, 380);
-            this.btnFavourites.Name = "btnFavourites";
-            this.btnFavourites.Size = new System.Drawing.Size(200, 60);
-            this.btnFavourites.TabIndex = 6;
-            this.btnFavourites.Text = " Favourites";
-            this.btnFavourites.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnFavourites.UseVisualStyleBackColor = true;
-            // 
-            // btnAddRecipe
-            // 
-            this.btnAddRecipe.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAddRecipe.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnAddRecipe.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
-            this.btnAddRecipe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAddRecipe.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddRecipe.ForeColor = System.Drawing.Color.White;
-            this.btnAddRecipe.Image = global::RecipesCatalog.Properties.Resources.chef;
-            this.btnAddRecipe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAddRecipe.Location = new System.Drawing.Point(0, 320);
-            this.btnAddRecipe.Name = "btnAddRecipe";
-            this.btnAddRecipe.Size = new System.Drawing.Size(200, 60);
-            this.btnAddRecipe.TabIndex = 5;
-            this.btnAddRecipe.Text = " Add Recipe";
-            this.btnAddRecipe.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnAddRecipe.UseVisualStyleBackColor = true;
-            // 
-            // btnCourses
-            // 
-            this.btnCourses.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnCourses.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnCourses.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
-            this.btnCourses.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCourses.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCourses.ForeColor = System.Drawing.Color.White;
-            this.btnCourses.Image = global::RecipesCatalog.Properties.Resources.donuts;
-            this.btnCourses.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCourses.Location = new System.Drawing.Point(0, 260);
-            this.btnCourses.Name = "btnCourses";
-            this.btnCourses.Size = new System.Drawing.Size(200, 60);
-            this.btnCourses.TabIndex = 4;
-            this.btnCourses.Text = " Deserts";
-            this.btnCourses.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnCourses.UseVisualStyleBackColor = true;
-            // 
-            // btnMain
-            // 
-            this.btnMain.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnMain.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnMain.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
-            this.btnMain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMain.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMain.ForeColor = System.Drawing.Color.White;
-            this.btnMain.Image = global::RecipesCatalog.Properties.Resources.meals;
-            this.btnMain.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMain.Location = new System.Drawing.Point(0, 200);
-            this.btnMain.Name = "btnMain";
-            this.btnMain.Size = new System.Drawing.Size(200, 60);
-            this.btnMain.TabIndex = 3;
-            this.btnMain.Text = " Main Courses";
-            this.btnMain.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnMain.UseVisualStyleBackColor = true;
-            // 
-            // btnAppetizers
-            // 
-            this.btnAppetizers.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAppetizers.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnAppetizers.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
-            this.btnAppetizers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAppetizers.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAppetizers.ForeColor = System.Drawing.Color.White;
-            this.btnAppetizers.Image = global::RecipesCatalog.Properties.Resources.fish_and_chips;
-            this.btnAppetizers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAppetizers.Location = new System.Drawing.Point(0, 140);
-            this.btnAppetizers.Name = "btnAppetizers";
-            this.btnAppetizers.Size = new System.Drawing.Size(200, 60);
-            this.btnAppetizers.TabIndex = 2;
-            this.btnAppetizers.Text = " Appetizers";
-            this.btnAppetizers.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnAppetizers.UseVisualStyleBackColor = true;
-            // 
-            // btnSalads
-            // 
-            this.btnSalads.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnSalads.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnSalads.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(94)))), ((int)(((byte)(93)))));
-            this.btnSalads.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSalads.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalads.ForeColor = System.Drawing.Color.White;
-            this.btnSalads.Image = global::RecipesCatalog.Properties.Resources.salad;
-            this.btnSalads.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSalads.Location = new System.Drawing.Point(0, 80);
-            this.btnSalads.Name = "btnSalads";
-            this.btnSalads.Size = new System.Drawing.Size(200, 60);
-            this.btnSalads.TabIndex = 1;
-            this.btnSalads.Text = " Salads";
-            this.btnSalads.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnSalads.UseVisualStyleBackColor = true;
-            this.btnSalads.Click += new System.EventHandler(this.btnSalads_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Segoe Print", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Image = global::RecipesCatalog.Properties.Resources.check_list__1_;
-            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label1.Location = new System.Drawing.Point(3, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 56);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "    Catalog";
             // 
             // Form1
             // 
